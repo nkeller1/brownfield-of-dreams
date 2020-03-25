3c01330b32e79ec201f6a4e0ddadbef183a2223d
Nathan Keller <keller.nathan@gmail.com>
2020-03-24 18:26:08 -0600

    update user controller to redirect correctly for an e mail in use

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
-      render :new
+      redirect_to "/register"
     end
   end
 

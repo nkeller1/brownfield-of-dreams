43189c34304ce83da2c8750f68c6621b665f44f2
Nathan Keller <keller.nathan@gmail.com>
2020-03-24 18:25:16 -0600

    Add sad path test for user creation

--- spec/features/user/new_spec.rb
+++ spec/features/user/new_spec.rb
@@ -25,4 +25,35 @@ describe 'A new user can be created' do
 
     expect(current_path).to eq('/dashboard')
   end
+
+  it "errors if the username is already taken" do
+    User.destroy_all
+
+    visit '/register'
+
+    fill_in 'user_email', with: "example@example.com"
+    fill_in 'user_first_name', with: "Joe"
+    fill_in 'user_last_name', with: "Schmo"
+    fill_in 'user_password', with: "password"
+    fill_in 'user_password_confirmation', with: "password"
+
+    click_on "Create Account"
+
+    expect(current_path).to eq('/dashboard')
+
+    click_on "Log Out"
+
+    visit '/register'
+
+    fill_in 'user_email', with: "example@example.com"
+    fill_in 'user_first_name', with: "Joe"
+    fill_in 'user_last_name', with: "Schmo"
+    fill_in 'user_password', with: "password"
+    fill_in 'user_password_confirmation', with: "password"
+
+    click_on "Create Account"
+
+    expect(page).to have_content('Username already exists')
+    expect(current_path).to eq('/register')
+  end
 end

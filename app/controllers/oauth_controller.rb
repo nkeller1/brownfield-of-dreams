e1b78ef82511532f99e62fbbda6a0937c10ceda6
Nathan Keller <keller.nathan@gmail.com>
2020-03-26 15:53:43 -0600

    Add test for oauth through github
    Co-Authored-By: Steve Anderson <steven.a.anderson2012@gmail.com>

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,5 +1,6 @@
 class OauthController < ApplicationController
   def create
+    # require "pry"; binding.pry
     current_user.update(token: request.env['omniauth.auth']['credentials']['token'])
 
     redirect_to dashboard_path, notice: "Connected to Github!"

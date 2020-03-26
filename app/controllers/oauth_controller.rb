22f6c9f72f839191ff6c382c05d57415985bbea6
Nathan Keller <keller.nathan@gmail.com>
2020-03-26 15:57:54 -0600

    remove extra pry in oauthcontroller

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,6 +1,5 @@
 class OauthController < ApplicationController
   def create
-    # require "pry"; binding.pry
     current_user.update(token: request.env['omniauth.auth']['credentials']['token'])
 
     redirect_to dashboard_path, notice: "Connected to Github!"

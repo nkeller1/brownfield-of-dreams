8c5bd386c06f5a002779abd8d67dad8c34fc0915
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-27 11:18:39 -0600

    Add full email authentication for registering users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
   default :from => 'no_reply@admin.com'
 
   def registration_confirmation(user)
-    @user = current_user
-    mail(:to => "#{user.name} <#{user.email}, :subject => 'Registration Confirmation for Dirtfield of Dreams'")
+    @user = user
+    require 'pry'; binding.pry
+    mail(:to => "#{@user.first_name} <#{@user.email}, :subject => 'Registration Confirmation for Dirtfield of Dreams'")
   end
 end

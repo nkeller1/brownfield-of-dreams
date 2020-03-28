fc1e19158df38faecab236d54d1efc3c9404e6f0
Nathan Keller <keller.nathan@gmail.com>
2020-03-28 11:26:01 -0600

    add test so a video is deleted when a tutoral is deleted

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -17,4 +17,23 @@ feature "An admin can delete a tutorial" do
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
+
+  scenario "and its videos no longer exist" do
+    admin = create(:admin)
+    tutorial = create(:tutorial)
+
+    video = create(:video, tutorial: tutorial )
+    # require "pry"; binding.pry
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/admin/dashboard"
+
+    expect(page).to have_css('.admin-tutorial-card', count: 1)
+
+    within(first('.admin-tutorial-card')) do
+      click_link 'Delete'
+    end
+
+    expect { Video.find(video.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+  end
 end

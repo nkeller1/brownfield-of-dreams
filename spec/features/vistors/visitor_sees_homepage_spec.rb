63b292bfa301b68300b9be94f91b590439232423
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-28 11:23:09 -0600

    Added more comprehensive test suite for bug fix

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe 'Visitor' do
   describe 'on the home page' do
-    it 'can see a list of tutorials' do
+    it 'as a user can see a list of tutorials' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       tutorial1 = create(:tutorial)
       tutorial2 = create(:tutorial)
 
@@ -15,6 +18,24 @@ describe 'Visitor' do
 
       expect(page).to have_css('.tutorial', count: 2)
 
+      within(first('.tutorials')) do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+      end
+    end
+
+    it 'can see non-classroom videos only' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial, classroom: true)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 1)
       within(first('.tutorials')) do
         expect(page).to have_css('.tutorial')
         expect(page).to have_css('.tutorial-description')

14d9b1e5f6f34d582d31ab6ba7306ec77f15e3c4
Nathan Keller <keller.nathan@gmail.com>
2020-03-31 08:49:15 -0600

    Create friendship migration and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :first_name
   validates_presence_of :last_name

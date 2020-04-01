2e18d965f5f0009e0da94a565fca53cf04deb737
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-31 18:27:37 -0600

    Remove comments and Change domain in smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 '587',
-    domain:               'example.com',
+    domain:               'dirtfield-of-dreams.herokuapp.com',
     user_name:            ENV["SENDGRID_USERNAME"],
     password:             ENV["SENDGRID_PASSWORD"],
     authentication:       'plain',

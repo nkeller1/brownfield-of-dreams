699d4bb689ae31ef884b57233c8a7968bb7fb33d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-31 18:35:02 -0600

    Fix domain issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 '587',
-    domain:               'dirtfield-of-dreams.herokuapp.com',
+    domain:               'https://dirtfield-of-dreams.herokuapp.com',
     user_name:            ENV["SENDGRID_USERNAME"],
     password:             ENV["SENDGRID_PASSWORD"],
     authentication:       'plain',

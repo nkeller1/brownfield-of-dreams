bf02c70f73024b2a97298b6e8dfe9353dab7e667
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-02 10:42:32 -0600

    Abstract away methods in mailers

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -8,16 +8,22 @@ class InvitesController < ApplicationController
     new_invite = InviteSearch.new(handle, token)
 
     email = new_invite.invitee_email
-    name = new_invite.invitee_name
+    invitee_name = new_invite.invitee_name
     inviter = current_user.username
 
-    if email.nil?
-      flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
-    else
-      InviteMailer.invite(name, email, inviter).deliver_now
-      flash[:success] = 'Successfully sent invite!'
-    end
+    send_invitation(email, invitee_name, inviter)
 
     redirect_to dashboard_path
   end
+
+  private
+
+    def send_invitation(email, invitee_name, inviter)
+      if email.nil?
+        flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
+      else
+        InviteMailer.invite(invitee_name, email, inviter).deliver_now
+        flash[:success] = 'Successfully sent invite!'
+      end
+    end
 end
